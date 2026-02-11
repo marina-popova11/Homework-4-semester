52c727e75013c895d73c00bc51a95a14501285ff
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-02-11 17:16:24 +0500

    add search a number

---
+++ Homework1/SearchForNumber/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let search target list =
+    let rec find index lst = 
+        match lst with
+        | [] -> None
+        | x::xs ->
+            if x = target then Some index
+            else find (index + 1) xs
+    find 0 list
+    
+
+let readArrayFromConsole() =
+    printfn "Enter the numbers in array"
+    let input = Console.ReadLine()
+    input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> Array.toList
+
+let list= readArrayFromConsole()
+printfn "Enter the number for search: "
+let number = Console.ReadLine()
+match Int32.TryParse(number) with
+| (true, value) -> 
+    let position = search value list
+    match position with
+        | None -> printfn "Element not found"
+        | Some index -> printfn "%d" index
+| (false, _) -> printfn "Invalid input"
