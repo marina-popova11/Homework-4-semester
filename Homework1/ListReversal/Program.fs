ecfd49049e8bc6817d63dd63974e98ed42ba99a7
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-02-12 11:33:34 +0500

    add list reversal

---
+++ Homework1/ListReversal/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let reverse list =
+    let rec loop acc lst =
+        match lst with
+        | [] -> acc
+        | x :: xs -> loop (x :: acc) xs
+    loop [] list 
+
+let readArrayFromConsole() =
+    printfn "Enter the symbols in array"
+    let input = Console.ReadLine()
+    input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+
+let list = readArrayFromConsole()
+let newList= reverse list
+printfn "%A" newList
