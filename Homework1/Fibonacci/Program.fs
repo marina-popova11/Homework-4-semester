587fd40f655b89d4b18bd39e077dc1b9fd84144a
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-02-13 13:21:58 +0500

    Added tests for fibonacci

--- Homework1/Fibonacci/Program.fs
+++ Homework1/Fibonacci/Program.fs
@@ -1,19 +1,17 @@
-﻿open System
+﻿module Program
 
-let rec fibonacci n =
-    if n < 0 then None
-    else
-        let rec loop i a b  = 
-            if i = n then Some a
-            else
-                loop (i + 1) b (a + b)
-        loop 0 0 1
+open System
+// open FibonacciLib
+open Fibonacci
 
-printfn "Enter the number: "
-try
-    let number = Int32.Parse(Console.ReadLine())
-    let result = fibonacci number
-    match result with
-    | None -> failwith "The number should be more than zero!"
-    | Some a -> printfn "Fibonacci number with index %d - %d" number a
-with ex -> printfn "%s" ex.Message
+[<EntryPoint>]
+let main _ =
+    printfn "Enter the number: "
+    try
+        let number = Int32.Parse(Console.ReadLine())
+        let result = fibonacci number
+        match result with
+        | None -> failwith "The number should be more than zero!"
+        | Some a -> printfn "Fibonacci number with index %d - %d" number a
+    with ex -> printfn "%s" ex.Message
+    0
