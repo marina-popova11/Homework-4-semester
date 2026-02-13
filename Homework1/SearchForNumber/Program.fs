ea8c48b9af2cf6dc5404f9a6bd028805ec76d5e1
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-02-13 14:43:34 +0500

    added tests for search

--- Homework1/SearchForNumber/Program.fs
+++ Homework1/SearchForNumber/Program.fs
@@ -1,29 +1,30 @@
-﻿open System
+﻿module Program
 
-let search target list =
-    let rec find index lst = 
-        match lst with
-        | [] -> None
-        | x::xs ->
-            if x = target then Some index
-            else find (index + 1) xs
-    find 0 list
+open System
+open Search
     
+[<EntryPoint>]
+let main _ =
+    let readArrayFromConsole() =
+        printfn "Enter the numbers in array"
+        let input = Console.ReadLine()
+        input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.choose (fun s ->
+            match Int32.TryParse(s) with
+            | (true, n) -> Some n
+            | (false, _) -> 
+                printfn "Warning: '%s' is not a number and will be skipped." s
+                None)
+        |> Array.toList
 
-let readArrayFromConsole() =
-    printfn "Enter the numbers in array"
-    let input = Console.ReadLine()
-    input.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-    |> Array.map int
-    |> Array.toList
-
-let list= readArrayFromConsole()
-printfn "Enter the number for search: "
-let number = Console.ReadLine()
-match Int32.TryParse(number) with
-| (true, value) -> 
-    let position = search value list
-    match position with
-        | None -> printfn "Element not found"
-        | Some index -> printfn "%d" index
-| (false, _) -> printfn "Invalid input"
+    let list= readArrayFromConsole()
+    printfn "Enter the number for search: "
+    let number = Console.ReadLine()
+    match Int32.TryParse(number) with
+    | (true, value) -> 
+        let position = search value list
+        match position with
+            | None -> printfn "Element not found"
+            | Some index -> printfn "Index: %d" index
+    | (false, _) -> printfn "Invalid input"
+    0
