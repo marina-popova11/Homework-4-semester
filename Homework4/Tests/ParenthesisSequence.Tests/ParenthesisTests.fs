537b58faf198e2ec3fa429cb9eff8d8d61a82064
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-03-04 21:37:51 +0300

    Added brackets and tests

---
+++ Homework4/Tests/ParenthesisSequence.Tests/ParenthesisTests.fs
@@ -0,0 +1,17 @@
+﻿module ParenthesisSequence.Tests
+
+open NUnit.Framework
+open CheckingNumber
+
+[<Test>]
+let Test_WithCorrectSeq () =
+    let seq = "(ggfhkkw(hhee[fnnrr]))"
+    let result = isValid(seq)
+    Assert.That(result, Is.True)
+
+[<Test>]
+let Test_WithOnlyBrackets () =
+    let seq = "([({})]{[])"
+    let result = isValid(seq)
+    Assert.That(result, Is.False)
+
