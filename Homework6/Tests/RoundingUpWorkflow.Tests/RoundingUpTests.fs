19d4aacb01cf9f7da4c39be71f0e52e58bc1b53a
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-03-19 21:43:17 +0300

    Added tests

---
+++ Homework6/Tests/RoundingUpWorkflow.Tests/RoundingUpTests.fs
@@ -0,0 +1,31 @@
+﻿module RoundingUpWorkflow.Tests
+
+open NUnit.Framework
+open RoundingUp
+
+[<Test>]
+let Test_RoundingTo3 () =
+    let rounding = RoundingBuilder(3)
+    let withRounding = rounding {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    Assert.That(withRounding, Is.EqualTo(0.048))
+
+[<TestCase(-0.123456, 2, -0.12)>]
+[<TestCase(-0.999, 1, -1.0)>]
+let Test_RoundingWithNegativeNumbers (input: float, n: int, result: float) =
+    let rounding = RoundingBuilder(n)
+    let withRounding = rounding {
+        return input
+    }
+    Assert.That(withRounding, Is.EqualTo(result))
+
+[<Test>]
+let Test_WithNegativeAccuracy () =
+    let rounding = RoundingBuilder(-2)
+    let withRounding = rounding {
+        return 0.128
+    }
+    Assert.That(withRounding, Is.EqualTo(float 0))
