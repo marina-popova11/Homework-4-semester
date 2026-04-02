9558fbccdf53c912bceea05b5c196035fbda6b33
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-04-02 17:08:49 +0300

    Added test for task 3

--- Test1/Tests/Task3.Tests/Tests.fs
+++ Test1/Tests/Task3.Tests/Tests.fs
@@ -1,11 +1,28 @@
-﻿module Task3.Tests
+﻿// <copyright file="Tests.fs" company="_">
+// Marina Popova, 2026, under MIT License.
+// </copyright>
+
+module Task3.Tests
 
 open NUnit.Framework
+open HashTableClass
 
-[<SetUp>]
-let Setup () =
-    ()
+let intHash (x: int) = x
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let Test_AddAndContains () =
+    let table = new HashTable<int>(intHash, 10)
+    table.Add(42)
+    Assert.That(table.Belongs(42), Is.True)
+
+[<Test>]
+let Test_DeleteExistingElement () =
+    let table = new HashTable<int>(intHash, 10)
+    table.Add(42)
+    Assert.That(table.Delete(42), Is.True)
+
+[<Test>]
+let Test_DeleteNotExistingElement () =
+    let table = new HashTable<int>(intHash, 10)
+    table.Add(42)
+    Assert.That(table.Delete(10), Is.False)
