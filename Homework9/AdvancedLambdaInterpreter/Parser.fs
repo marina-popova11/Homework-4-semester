16ffa8134fc452133e733ae84bddb8b9f9363964
marina-popova11 <181482786+marina-popova11@users.noreply.github.com>
2026-04-19 12:44:08 +0300

    added functions of elements and terms

---
+++ Homework9/AdvancedLambdaInterpreter/Parser.fs
@@ -0,0 +1,60 @@
+module Parser
+
+open System
+open FParsec
+open Interpreter
+
+type InputObjects =
+    | Definition of (string * LambdaTerm) list
+    | Expression of LambdaTerm
+
+let spaces: Parser<unit, unit> = skipMany (pchar ' ' <|> pchar '\t' <|> pchar '\n')
+
+let identifier =
+    let isChar c = System.Char.IsLetterOrDigit c || c = '_'
+    many1Satisfy2L System.Char.IsLetter isChar "identifier"
+let pEquality = pchar '=' .>> spaces
+
+let pLet = pstring "let" .>> spaces
+
+let pSlash = pchar '\\' .>> spaces
+
+let pDot = pchar '.' .>> spaces
+
+let pTerm, pTermImpl = createParserForwardedToRef()
+
+let pAtomTerm =
+    choice [
+        between (pchar '(' .>> spaces) (pchar ')' .>> spaces) pTerm
+        identifier |>> Var
+    ]
+
+let pApp =
+    chainl1 pAtomTerm (spaces >>. preturn (fun f g -> App (f, g)))
+
+let pLambda =
+    pSlash >>. many1 (identifier .>> spaces) >>= fun args ->
+        pDot >>. pTerm |>> fun body ->
+            List.foldBack (fun arg acc -> Abs (arg, acc)) args body
+
+let pDefinition =
+    pLet >>. identifier .>> pEquality .>>. pTerm |>> fun (name, term) -> (name, term)
+
+let pProgram =
+    let pLines =
+        choice [
+            attempt (pDefinition |>> fun def -> Choice1Of2 def)
+            (pTerm |>> fun t -> Choice2Of2 t)
+        ]
+    
+    let pDefs = many (pDefinition .>> spaces)
+    
+    pDefs .>>. (pTerm .>> spaces .>> eof) |>> fun (defs, main) ->
+    { Definition = defs; Expression = main }
+
+let parseString (str: string) =
+    run pProgram str
+
+let parseFile (filePath: string) =
+    let body = System.IO.File.ReadAllText(filePath)
+    run pProgram body
